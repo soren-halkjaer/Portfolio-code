a070438d5871d485e611c00a8c8f6a23508b62e4
Søren Halkjær <soren.halkjar@gmail.com>
2025-12-19 11:21:22 +0100

    This commit is to ensure that I can work on the latest version in Turckheim. I don't think significant changes have been made.

---
+++ nordnet_stock_ID.py
@@ -0,0 +1,17 @@
+nordnet_stock_ID = {
+    'NOVO-B.CO': ["Novo Nordisk B", '16256554'],                                                        # 150
+    'PNDORA.CO': ["Pandora", '16101996'],                                                               # 70
+    'AOJ-B.CO':  ["A & O Johansen A/S", '384b908d-d07b-403e-837b-2a97900b23ee'],                        # 1117
+    'DNORD.CO':  ["D/S Norden", '4d7007dd-6cc8-42ce-81e6-5c08e3fc8e42'],                                # 425
+    'DEMANT.CO': ["Demant A/S", 'bf111a08-d545-449d-add7-4783af800242'],                                # 340
+    'RILBA.CO':  ["Ringkjøbing Landbobank", '538aba9e-5b66-45d0-a89e-17a6f4b33670'],                    # 75
+    'RBREW.CO':  ["Royal UNIBREW", 'a6250dce-69d7-40c8-a743-838f10a1861f'],                             # 180
+    'STG.CO':    ["Scandinavian Tobacco Group",'ca165109-7443-4e95-b173-ee27b62ce817'],                 # 950
+    'SCHO.CO':   ["Schouw & Co.", 'e99b41e6-0380-479d-b0a0-e524c62a1b1d'],                              # 170
+    'COLO-B.CO': ["Coloplast", '00a29a7b-107c-40b3-a852-e5f25045e9e6'],                                 # 115
+    'GMAB.CO':   ["Genmab", '2faa56a8-107b-432a-8dbe-48d2e5c3ffc1'],                                    # 53
+    'SPG.CO':    ["SP Group", '35f52d4c-8542-4874-a793-19b26646bfe8'],                                   # 219
+    'DSV.CO':    ["DSV A/S", '6cb53a21-593b-4cbb-8b77-01cfcd0714c0'],
+    'UIE.CO':    ["UIE", 'f6b4731c-a01d-4499-88fa-94085595b1a7'],
+    'OMXC25GI':  ["OMXC25GI", "OMXC25GI"]                                                                          # 260
+    }
